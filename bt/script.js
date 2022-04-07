9eecaa61dcc042ee99ffe8540c40b04a2a48c6e9
Robert Spier <robert.spier@gmail.com>
2022-04-07 10:56:14 +0200

    naar excel

---
+++ bt/script.js
@@ -0,0 +1,17 @@
+const main = document.querySelector('main');
+
+fetch('https://opensheet.elk.sh/1igRwZ_HCmEoMpqgMNWrBalOgcMSJItVK1DF-GSovweY/bt')
+	.then(data => {
+		const ul = document.createElement('ul');
+		data.forEach(item => {
+			const li = document.createElement('li');
+			const link = document.createElement('a');
+			link.setAttribute('href', item.url)
+			link.textContent = item.naam
+			li.appendChild(link);
+			ul.appendChild(li)
+		})
+	})
+	.catch(err => {
+		main.innerHTML = 'kapot, jammer, probeer het later nog eens'
+	})
