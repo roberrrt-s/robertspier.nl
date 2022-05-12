d10b28a0fee05c91a23124ed04d4c783fbabc723
Robert Spier <robert.spier@gmail.com>
2022-05-12 14:29:22 +0200

    update

--- sounds-like-trouble/js/main.js
+++ sounds-like-trouble/js/main.js
@@ -1,1 +1,15 @@
+let button = document.querySelector('button');
+let cookies = document.querySelector('div');
 
+button.addEventListener('click', removeCookies)
+
+console.log(document.cookie)
+
+if(!document.cookie) {
+	cookies.classList.add('is-visible');
+}
+
+function removeCookies() {
+	document.cookie = "clicked=true";
+	cookies.classList.add('is-closed');
+}
